1f2066d0c1ca2b05a1647c138771ecad3b599778
Calvin Collins <calvinkcollins@gmail.com>
2016-03-03 01:09:23 -0500

    switched course to experimental website

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask, render_template
+from flask.ext import assets
 
 app = Flask(__name__)
+
+env = assets.Environment(app)
+
+# Tell flask-assets where to look for our coffeescript and sass files.
+env.load_path = [
+    os.path.join(os.path.dirname(__file__), 'sass'),
+]
+
+env.register(
+    'main',
+    assets.Bundle(
+        'main.sass',
+        filters='sass',
+        output='css/main.css'
+    )
+)
 
 @app.route('/')
 def main():
