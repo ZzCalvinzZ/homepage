b3fd1cc6a4e391000129ec1198ffafe92c8154e2
Calvin Collins <calvinkcollins@gmail.com>
2016-03-06 23:01:36 -0500

    separate js files

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,3 @@
 $(document).ready(function () {
 
-	var name = '';
-
-	$('.challenge-box').on('input', 'input:text', function () {
-		var that = $(this);
-		name = that.val();
-
-		var button = that.parent().siblings('button');
-
-		if (name){
-			button.show().text(name + '?');
-		} else {
-			button.hide().text('');
-		}
-
-
-	});
-
-
 });
