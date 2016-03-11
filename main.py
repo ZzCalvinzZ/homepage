94744d0abbc1463a4ad9ca7557215f4a6fb0ec52
Calvin Collins <calvinkcollins@gmail.com>
2016-03-10 23:12:58 -0500

    factor out projects

--- main.py
+++ main.py
@@ -8,33 +8,43 @@ env = assets.Environment(app)
 
 # Tell flask-assets where to look for our coffeescript and sass files.
 env.load_path = [
-    os.path.join(os.path.dirname(__file__), 'sass'),
+	os.path.join(os.path.dirname(__file__), 'sass'),
 ]
 
 env.register(
-    'home',
-    assets.Bundle(
-        'home/main.sass',
-        filters='sass',
-        output='css/home.css'
-    )
+	'home',
+	assets.Bundle(
+		'home/main.sass',
+		filters='sass',
+		output='css/home.css'
+	)
 )
 env.register(
-    'cklabs',
-    assets.Bundle(
-        'cklabs/main.sass',
-        filters='sass',
-        output='css/cklabs.css'
-    )
+	'cklabs',
+	assets.Bundle(
+		'cklabs/main.sass',
+		filters='sass',
+		output='css/cklabs.css'
+	)
 )
 
 @app.route('/')
 def main():
-	return render_template('home.html')
+	projects = [{
+			'name': 'Minesweeper',
+			'link': 'static/img/portfolio/minesweeper.png',
+		}, {
+			'name': "Gascreep's Fortune",
+			'link': "static/img/portfolio/gascreep.png" ,
+		}
+			
+	]
+
+	return render_template('home.html', projects=projects)
 
 @app.route('/cklabs')
 def experimental():
 	return render_template('cklabs.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+	app.run(host='0.0.0.0', debug=True)
