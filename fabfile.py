34355f6fa9d42a92cbfd75610d12719df151b70a
Calvin Collins <calvinkcollins@gmail.com>
2016-10-26 22:59:11 -0400

    added fabfile

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+from fabric.api import run, cd, sudo, hosts
+
+@hosts('calvin@calvinkcollins.com')
+def update(branch=None):
+	base = '/var/www/calvinkcollins/homepage'
+	python = '%s/.env/bin/python' % base
+	workon = 'source .env/bin/activate &&'
+
+	with cd(base):
+
+		run('git fetch')
+
+		if branch:
+			run('git checkout %s' % branch)
+
+		run('git pull')
+
+		run('%s pip install -r requirements.txt' % workon)
+		run('find . -name "*.pyc" -delete')
+
+		sudo('service nginx restart')
+		sudo('service uwsgi restart')
+
